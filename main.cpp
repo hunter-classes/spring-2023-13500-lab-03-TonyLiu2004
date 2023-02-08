db704fe9c4a1c758b7246d8708f4dcf2c7cd81b2
Tonyliu2004 <ltsplice@gmail.com>
2023-02-08 16:11:54 -0500

    test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 // add your code as appropriate 
 #include <iostream>
+#include "reservoir.h"
 
 int main()
 {
-  
-
+  int a = test();
+  std::cout << a;//get_east_storage("05/10/2018");
   
   return 0;
 }
